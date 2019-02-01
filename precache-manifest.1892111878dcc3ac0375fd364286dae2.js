e30c0051dd49e190bca7a4d7fe1b95fdb29267f1
kong0107 <kong0107@gmail.com>
2019-02-01 23:34:45 +0800

    Updates

---
+++ precache-manifest.1892111878dcc3ac0375fd364286dae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8583d953808fad433ff",
+    "url": "/mojLaw/static/js/runtime~main.e8583d95.js"
+  },
+  {
+    "revision": "ddac91ffd2824cbac729",
+    "url": "/mojLaw/static/js/main.ddac91ff.chunk.js"
+  },
+  {
+    "revision": "f7cd3c50c507290e6267",
+    "url": "/mojLaw/static/js/1.f7cd3c50.chunk.js"
+  },
+  {
+    "revision": "ddac91ffd2824cbac729",
+    "url": "/mojLaw/static/css/main.07cc5d97.chunk.css"
+  },
+  {
+    "revision": "1eecf4329e7348d821d687c53c78aa7a",
+    "url": "/mojLaw/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mojLaw/precache-manifest.2e6aa67d29244ba9dabc7e7d94cb15cd.js"
+  "/mojLaw/precache-manifest.1892111878dcc3ac0375fd364286dae2.js"
 );
 
 workbox.clientsClaim();
